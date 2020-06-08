f70128d243c539f32277b591e5aba006e9004b5e
lix.digito <lix.digito@hotmail.com>
2020-06-07 21:17:21 -0300

    implementacao

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreatTransactionDto {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,29 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.reduceValueByType('income');
+    const outcome = this.reduceValueByType('outcome');
+    const total = income - outcome;
+
+    return {income, outcome, total};
   }
 
-  public create(): Transaction {
-    // TODO
+  private reduceValueByType(type: 'income' | 'outcome') : number {
+    return this.transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === type) return accumulator += currentValue.value;
+      return accumulator;
+    }, 0);
+  }
+
+  public create({title, value, type} : CreatTransactionDto): Transaction {
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+    return transaction;
+    
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,12 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type} : Request): Transaction {
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      if (value > balance.total) throw Error('Total is insufficient for outcome.');
+    }
+    return this.transactionsRepository.create({title, value, type});
   }
 }
 
